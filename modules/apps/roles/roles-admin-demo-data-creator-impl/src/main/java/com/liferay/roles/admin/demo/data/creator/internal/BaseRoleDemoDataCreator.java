db420cfac90c733ceec8946e55f4afa6ef35d614
Davi Santos <davi.santos@liferay.com>
2023-10-30 10:09:28 -0300

    test

--- modules/apps/roles/roles-admin-demo-data-creator-impl/src/main/java/com/liferay/roles/admin/demo/data/creator/internal/BaseRoleDemoDataCreator.java
+++ modules/apps/roles/roles-admin-demo-data-creator-impl/src/main/java/com/liferay/roles/admin/demo/data/creator/internal/BaseRoleDemoDataCreator.java
@@ -5,7 +5,7 @@
 
 package com.liferay.roles.admin.demo.data.creator.internal;
 
-import com.liferay.portal.kernel.exception.PortalException;
+import com.sssliferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Company;
